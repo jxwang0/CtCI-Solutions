c650a6f1bef124da5e6f07052c8566e625dff51d
unknown <jxwang0@gmail.com>
2015-09-08 17:00:15 -0500

    5/5

---
+++ 5/One.java
@@ -0,0 +1,34 @@
+public class One
+{
+	public static boolean getBit(int num, int i)
+	{
+		return ((num & (1 << i)) != 0);
+	}
+
+	public static int setBitOne(int num, int i)
+	{
+		return num | (1 <<i);
+	}
+	public static int setBitZero(int num, int i)
+	{
+		return num | (0 <<i);
+	}
+
+	public static int insertion (int insertee, int inserter, int start, int end)
+	{
+		int num = insertee;
+		for (int i = start; i <= end; i++)
+		{
+				if (getBit(insertee, i - start))
+					num = setBitOne(num, i);
+				else
+					num = setBitZero(num, i);
+		}
+		return num;
+	}
+
+	public static void main (String [] args)
+	{
+		System.out.println(insertion(Integer.parseInt("10000000000",2), Integer.parseInt("10011", 2), 2, 6));
+	}
+}
---
+++ One.java
@@ -0,0 +1,12 @@
+public class One
+{
+	public static boolean getBit(int num, int i)
+	{
+		return ((num & (1 << i)) !=);
+	}
+
+	public static int setBit(int num, int i)
+	{
+		return num | (!<<i);
+	}
+}
